a099b6658460e900102ce71c7912218a77d5d89b
iammurad <murad@nantech.az>
2022-09-11 20:38:19 +0400

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
