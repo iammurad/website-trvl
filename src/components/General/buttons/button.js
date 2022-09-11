568728687d6c944e40bf980eae20aca4e824baef
iammurad <murad@nantech.az>
2022-09-11 16:43:43 +0400

    style changes

--- src/components/General/buttons/button.js
+++ src/components/General/buttons/button.js
@@ -2,31 +2,32 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 
-const STYLES = ["btn--primary", "btn--outline"];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
 
-const SIZES = ["btn--medium", "btn--large"];
+const SIZES = ['btn--medium', 'btn--large'];
+
 export const Button = ({
   children,
   type,
   onClick,
   buttonStyle,
-  buttonSize,
+  buttonSize
 }) => {
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
-    : STYLE[0];
+    : STYLES[0];
+
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-    return(
-        <Link to="/sign-up" className="btn-mobile">
-            <button
-            className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            >
-                {children}
-            </button>
-        </Link>
-    )
-
-
-
+  return (
+    <Link to='/sign-up' className='btn-mobile'>
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 };
