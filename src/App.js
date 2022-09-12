53b75412b17185cf6e8f43b160978ecfeb1c4e1f
iammurad <murad@nantech.az>
2022-09-12 20:29:05 +0400

    Cards,Hero,Home added , some misc changes&&fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
     <Router>
     <Navbar/>
     <Routes>
-      <Route path='/' exact />
+      <Route path='/' exact  element={<Home />} />
     </Routes>
     </Router>
     </>
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import '../../App.css'
+import Cards from '../Card/Cards'
+import Hero from '../Hero/Hero'
+function Home() {
+    return(
+        <>
+            <Hero/>
+            <Cards/>
+        </>
+    )
+}
+export default Home
