5d2ebda2cca91ca0232204f6caa091b0e3fb08f0
iammurad <murad@nantech.az>
2022-09-15 22:00:18 +0400

    last changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -70,7 +70,7 @@ function Footer() {
               <i class='fab fa-typo3' />
             </Link>
           </div>
-          <small class='website-rights'>TRVL © 2020</small>
+          <small class='website-rights'>Murad Zeynalli © 2052</small>
           <div class='social-icons'>
             <Link
               class='social-icon-link facebook'
